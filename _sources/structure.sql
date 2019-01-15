19278566cceae9e71fa571b442f097946f543c6e
Jan Daniel Dolezal <dan@tealake.cz>
2019-01-15 13:39:48 +0100

    WIP change DB structure

--- _sources/structure.sql
+++ _sources/structure.sql
@@ -12,4 +12,43 @@ create table credentials (
 	foreign key (`user`) references users(id)
 );
 
+CREATE TABLE cars (
+	id varchar(16) primary key,
+	registration VARCHAR(9) NOT NULL,
+	`name` VARCHAR(50) NOT NULL,
+	display BOOL DEFAULT true
+);
 
+CREATE TABLE trips (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	overwriten_by INT NULL,
+
+	car varchar(16) NOT NULL,
+	`driver` VARCHAR(16) NOT NULL,
+	
+	added_by VARCHAR(16) NULL,
+	added_on DATETIME NULL,
+	removed_by VARCHAR(16) NULL,
+	removed_on DATETIME NULL,
+	
+	start_odometer INT NOT NULL,
+	start_place VARCHAR(50) NOT NULL,
+	start_date DATETIME NOT NULL,
+	
+	target_client VARCHAR(50) NULL,
+	target_place VARCHAR(50) NULL,
+	
+	end_odometer INT NULL,
+	end_place VARCHAR(50) NULL,
+	end_date DATETIME NULL,
+
+	is_personal BOOL NULL,
+	and_back BOOL NULL,
+
+	FOREIGN KEY (`overwriten_by`) REFERENCES trips(id),
+	FOREIGN KEY (`car`) REFERENCES cars(id),
+	FOREIGN KEY (`driver`) REFERENCES users(id),
+	
+	FOREIGN KEY (`added_by`) REFERENCES users(id),
+	FOREIGN KEY (`removed_by`) REFERENCES users(id)
+);
