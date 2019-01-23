a75e4c204b973716dfa5b1056af04a292c2a9bee
Jan Daniel Dolezal <dan@tealake.cz>
2019-01-23 10:38:26 +0100

    better access definition

--- app/Controller.php
+++ app/Controller.php
@@ -4,8 +4,12 @@
 namespace Journbers;
 
 
+use Tracy\Debugger;
+
 class Controller
 {
+    const ANY_ROLE = '86890c88-1375-4eeb-b47d-1cb7d6c4d305';
+
     protected $config = null;
     protected $request = null;
 
@@ -13,6 +17,39 @@ class Controller
     {
         $this->config = $config;
         $this->request = $request;
+    }
+
+    public function checkAccess($method)
+    {
+        Debugger::barDump($method);
+        Debugger::barDump(get_class($this));
+        $m = [];
+        preg_match('#Controller\\\(.*)$#', get_class($this), $m);
+        $shortClass = $m[1];
+        $accessKey = sprintf('%s::%s', ucfirst($shortClass), ucfirst($method));
+        Debugger::barDump($accessKey);
+        $access = $this->config()->get('access');
+        if (!isset($access[$accessKey]) || empty($access[$accessKey])) {
+            throw new \RuntimeException(sprintf('No allowed roles are set for %s action.', $accessKey));
+        }
+        $roles = $access[$accessKey];
+        if (!is_array($roles)) {
+            throw new \RuntimeException(
+                sprintf('Allowed roles for action %s should be defined as array in config.', $accessKey)
+            );
+        }
+
+        if (in_array(self::ANY_ROLE, $roles)) {
+            return true;
+        }
+
+        foreach ($roles as $allowedRole) {
+            if ($this->request()->user()->hasRole($allowedRole)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     protected function request()
@@ -60,5 +97,16 @@ class Controller
         $t->vars($this->commonTemplateVars());
 
         return $t;
+    }
+
+    public function messageAndRedirect($message, $path, $type = 'info')
+    {
+        if ($message !== null) {
+            $f = new Flash();
+            $f->message($message, $type);
+        }
+
+        $this->redirect($path);
+        $this->exit();
     }
 }
